a2e823d9267711142f937371e9636150832a0ac5
Andres Eloy <39571970+andrespd99@users.noreply.github.com>
2024-02-28 20:33:37 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,60 +1,23 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+$json = file_get_contents('php://input');
+$data = json_decode($json);
 
-define('LARAVEL_START', microtime(true));
+$from = "emergencyapp@tuvelozexpress.com";
+$to = $data->email;
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+$otp = $data->otp;
 
-require __DIR__.'/../vendor/autoload.php';
+$subject = "Verificación de OTP";
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+$message = "Tu código OTP es: $otp";
+$message = mb_convert_encoding($message, "UTF-8", "ISO-8859-1");
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$headers = "From:" . $from . "\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8\r\n";
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+mail($to, $subject, $message, $headers);
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+echo "Email sended";
